789842f53e825ceda9bf0973e2ea974db67d85f8
yutopp <yutopp@gmail.com>
2014-08-11 16:48:44 +0900

    configure mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # mailer
+  config.action_mailer.default_url_options = { :host => 'test.sc.yutopp.net' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                "127.0.0.1",
+    port:                   25,
+    enable_starttls_auto:   false
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,25 @@ Rails.application.routes.draw do
   match 'entries/:entry_id' => 'entries#show_entry', :via => :get
 
   #
+  match 'users' => 'users#index', :via => :get
+  match 'users/codes' => 'users#show_my_codes', :via => :get
+
+  #
   match 'api/source' => 'api#post_source', :via => :post
   match 'api/entry/:entry_id' => 'api#get_entry', :via => :get
   match 'api/ticket/:ticket_id' => 'api#get_ticket', :via => :get
   match 'api/nodes' => 'api#get_cage_nodes', :via => :get
+
+  #
+  # for master controller(like dashboard for admin)
+  match 'master' => 'master#index', :via => :get
+  match 'master/users' => 'master#list_users', :via => :get
+  match 'master/users/:id' => 'master#delete_user', :via => :delete
+  match 'master/update_proctable' => 'master#update_proctable', :via => :post
+  match 'master/runner_node_addresses' => 'master#update_node_addresses', :via => :post
+  match 'master/mask_proctable/:index' => 'master#mask_proctable', :via => :post
+  match 'master/unmask_proctable/:index' => 'master#unmask_proctable', :via => :post
+
+  #
+  match 'feedback/send' => 'feedback#send_mail', :via => :post
 end
