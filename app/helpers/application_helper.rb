4abb65195d459e6b8227fbe1669ae50f57f741cd
yutopp <yutopp@gmail.com>
2014-09-12 21:29:08 +0900

    fix navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def navbar_home?
+    return params[:controller] == "home"
+  end
+
+  def navbar_about?
+    return params[:controller] == "about"
+  end
+
+  def navbar_master?
+    return params[:controller] == "master"
+  end
+
+  def navbar_user?
+    return params[:controller] == "users" ||
+      ( params[:controller] == "users/registrations" && params[:action] == "edit" )
+  end
+
+  def navbar_register?
+    return params[:controller] == "users/registrations" && params[:action] != "edit"
+  end
+
+  def navbar_login?
+    return params[:controller] == "devise/sessions"
+  end
 end
