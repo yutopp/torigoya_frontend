923edea5d911ff80148ac6645702ed1ad850aa4e
yutopp <yutopp@gmail.com>
2014-09-12 21:31:33 +0900

    add lang proc filter

--- app/controllers/concerns/cages.rb
+++ app/controllers/concerns/cages.rb
@@ -2,19 +2,91 @@ require 'torigoya_kit'
 require 'singleton'
 
 module Cages
-  #
+  # returns proc table data
   def get_proc_table(platform = :linux)
-    # TODO: implement cache
     h = ProcTableHolder.instance
-    unless h.having?
-      session = Instances.instance.pick(platform)
-      pt = session.get_proc_table
-      h.set pt
+    unless h.cached?
+      update_proc_table(platform)
     end
 
     return h.table
   end
   module_function :get_proc_table
+
+  #
+  def update_proc_table(platform = :linux)
+    update_proc_table_from_node()
+    load_proc_table_from_db()
+  end
+  module_function :update_proc_table
+
+
+  #
+  def update_proc_table_from_node(platform = :linux)
+    # load data from a remote node
+    session = Instances.instance.pick(platform)
+    pt = session.get_proc_table
+
+    store_proc_table_to_db(pt)
+  end
+  module_function :update_proc_table_from_node
+
+  #
+  def store_proc_table_to_db(pt)
+    tmp = {}
+    LangProc.all.each do |lp|
+      tmp[lp.id] = { :proc_id => lp.proc_id, :version => lp.version }
+    end
+
+    pt.each do |proc_id, body|
+      body['Versioned'].each do |version, versioned_info|
+        begin
+          ts = LangProc.where(proc_id: proc_id, version: version)
+          lang_proc = ts.first
+          lang_proc.description = body['Description']
+          lang_proc.versioned_info = versioned_info
+          lang_proc.save!
+
+          tmp.delete(lang_proc.id)
+
+        rescue Mongoid::Errors::DocumentNotFound
+          lang_proc = LangProc.new({
+                                     :proc_id => proc_id,
+                                     :description => body['Description'],
+                                     :version => version,
+                                     :versioned_info => versioned_info
+                                   })
+          lang_proc.save!
+
+        end # begin
+      end # body['Versioned'].each
+    end # pt.each
+
+    tmp.each_key do |key|
+      LangProc.find(key).delete
+    end
+  end
+  module_function :store_proc_table_to_db
+
+  #
+  def load_proc_table_from_db()
+    h = ProcTableHolder.instance
+
+    table = {}
+    langprocs = LangProc.where({ :masked => false })
+    langprocs.each do |langproc|
+      unless table.has_key?(langproc.proc_id)
+        table[langproc.proc_id] = {
+          "Description" => langproc.description,
+          "Versioned" => {}
+        }
+      end
+      table[langproc.proc_id]["Versioned"][langproc.version] = langproc.versioned_info
+    end
+
+    h.set table
+  end
+  module_function :load_proc_table_from_db
 
   #
   def get_nodes_info
@@ -53,7 +125,7 @@ module Cages
   class ProcTableHolder
     include Singleton
 
-    def having?
+    def cached?
       return @table != nil
     end
 
