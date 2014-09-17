0caf7ac267f9f71370ac05d7320dedf365ad4196
yutopp <yutopp@gmail.com>
2014-09-17 18:38:18 +0900

    resolve #8

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,10 @@
 class AboutController < ApplicationController
   def index
+    versionfile_path = File.join(Rails.root, ".frontend_version")
+    @version = if File.exists?(versionfile_path) then
+                 File.open(File.join(Rails.root, ".frontend_version"), "r").read
+               else
+                 "(not versioned)"
+               end
   end
 end
